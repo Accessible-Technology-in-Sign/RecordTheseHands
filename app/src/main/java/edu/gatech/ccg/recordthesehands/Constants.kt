d1394be85d1fe72983cceddfd4a561a7c4f05638
mgeorg@google.com <mgeorg@google.com>
2025-11-03 13:40:43 -0600

    Updated version to v2.3.2

--- app/src/main/java/edu/gatech/ccg/recordthesehands/Constants.kt
+++ app/src/main/java/edu/gatech/ccg/recordthesehands/Constants.kt
@@ -27,7 +27,7 @@ object Constants {
   /**
    * The app version (used in server communications).
    */
-  const val APP_VERSION = "2.3.1"
+  const val APP_VERSION = "2.3.2"
 
   /**
    * The size, in inches, at which we should consider the user's device a tablet.
