786e477e66bbf628e4da31ee7cdc1e25487adde9
mgeorg@google.com <mgeorg@google.com>
2025-11-11 09:48:31 -0600

    Added fading to top and bottom of text in InstructionsActivity.

--- app/src/main/java/edu/gatech/ccg/recordthesehands/home/InstructionsActivity.kt
+++ app/src/main/java/edu/gatech/ccg/recordthesehands/home/InstructionsActivity.kt
@@ -3,9 +3,7 @@ package edu.gatech.ccg.recordthesehands.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,13 +14,15 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import edu.gatech.ccg.recordthesehands.R
 import edu.gatech.ccg.recordthesehands.ui.components.PrimaryButton
 
@@ -34,7 +34,6 @@ class InstructionsActivity : ComponentActivity() {
     setContent {
       InstructionsScreen(
         sectionName = sectionName,
-        onBackClick = { finish() },
         onContinueClick = { finish() }
       )
     }
@@ -44,7 +43,6 @@ class InstructionsActivity : ComponentActivity() {
 @Composable
 fun InstructionsScreen(
   sectionName: String,
-  onBackClick: () -> Unit,
   onContinueClick: () -> Unit
 ) {
   ConstraintLayout(
@@ -52,28 +50,18 @@ fun InstructionsScreen(
       .fillMaxSize()
       .background(Color.White)
   ) {
-    val (backButton, header, videoPlaceholder, instructionsText, continueButton) = createRefs()
-
-    Image(
-      painter = painterResource(id = R.drawable.back_arrow),
-      contentDescription = stringResource(R.string.back_button),
-      modifier = Modifier
-        .constrainAs(backButton) {
-          start.linkTo(parent.start, margin = 16.dp)
-          top.linkTo(parent.top, margin = 16.dp)
-        }
-        .clickable(onClick = onBackClick)
-    )
+    val (backButton, header, videoPlaceholder, instructionsText, instructionsTextTopFade, instructionsTextBottomFade, continueButton) = createRefs()
 
     Text(
       text = stringResource(R.string.instructions_for_section, sectionName),
       fontSize = 32.sp,
       fontWeight = FontWeight.Bold,
       modifier = Modifier.constrainAs(header) {
-        top.linkTo(backButton.bottom, margin = 16.dp)
-        start.linkTo(parent.start)
-        end.linkTo(parent.end)
-      }
+        top.linkTo(parent.top, margin = 16.dp)
+        start.linkTo(parent.start, margin = 16.dp)
+        end.linkTo(parent.end, margin = 16.dp)
+      },
+      textAlign = TextAlign.Center,
     )
 
     // Placeholder for Video
@@ -98,20 +86,57 @@ fun InstructionsScreen(
           start.linkTo(parent.start, margin = 16.dp)
           end.linkTo(parent.end, margin = 16.dp)
           bottom.linkTo(continueButton.top, margin = 16.dp)
+          height = Dimension.fillToConstraints
         }
         .verticalScroll(scrollState)
+        .fillMaxWidth()
     ) {
+
       Text(
         text = "These are the instructions for section $sectionName. ".repeat(50),
         fontSize = 18.sp
       )
+    }
+    if (scrollState.canScrollBackward) {
+      Box(
+        modifier = Modifier
+          .constrainAs(instructionsTextTopFade) {
+            top.linkTo(instructionsText.top, margin = 0.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            height = Dimension.value(100.dp)
+          }
+          .fillMaxWidth()
+          .background(
+            brush = Brush.verticalGradient(
+              colors = listOf(Color.White, Color.Transparent),
+            )
+          )
+      ) {}
+    }
+    if (scrollState.canScrollForward) {
+      Box(
+        modifier = Modifier
+          .constrainAs(instructionsTextBottomFade) {
+            bottom.linkTo(instructionsText.bottom, margin = 0.dp)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            height = Dimension.value(100.dp)
+          }
+          .fillMaxWidth()
+          .background(
+            brush = Brush.verticalGradient(
+              colors = listOf(Color.Transparent, Color.White),
+            )
+          )
+      ) {}
     }
 
     PrimaryButton(
       onClick = onContinueClick,
       text = "Continue",
       modifier = Modifier.constrainAs(continueButton) {
-        bottom.linkTo(parent.bottom, margin = 24.dp)
+        bottom.linkTo(parent.bottom, margin = 16.dp)
         start.linkTo(parent.start)
         end.linkTo(parent.end)
       }
